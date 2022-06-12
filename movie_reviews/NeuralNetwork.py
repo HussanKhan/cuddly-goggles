c7025e7620d5e12df18e457fe9275675dfef2df9
Hussan Khan <hussankhanwork@gmail.com>
2022-06-11 19:20:28 -0500

    working back prop

--- movie_reviews/NeuralNetwork.py
+++ movie_reviews/NeuralNetwork.py
@@ -15,8 +15,8 @@ class NeuralNetworkNLP:
         # Array content = [Row, Col]
         self.layerDims = layerDims
         
-        # first and last are always input and output
-        self.InputLayer = np.zeros(self.layerDims[0])
+        # first is always input vector
+        self.InputVector = np.zeros(self.layerDims[0])
         self.outputLayer = np.zeros(self.layerDims[-1])
         
         self.hiddenLayers = []
@@ -30,14 +30,17 @@ class NeuralNetworkNLP:
     def load_input_layer(self, input):
         
         # clear it
-        self.InputLayer *= 0
+        self.InputVector *= 0
         
         # load it
         for element in input.split(" "):
-            self.InputLayer[0][self.mapping[element]] += 1
+            self.InputVector[0][self.mapping[element]] += 1
             
     def sigmoid(self,x):
         return 1 / (1 + np.exp(-x))
+    
+    def sigmoid_derivative(self,output):
+        return output * (1 - output)
             
     def run(self, inputVector):
         
@@ -45,7 +48,7 @@ class NeuralNetworkNLP:
         self.load_input_layer(inputVector)
         
         # feed-forward
-        lastOutput = self.InputLayer
+        lastOutput = self.InputVector
         
         for i in range(len(self.hiddenLayers)):   
             lastOutput = lastOutput.dot(self.hiddenLayers[i])
@@ -80,3 +83,54 @@ class NeuralNetworkNLP:
                              + " #Correct:" + str(correct) + " #Tested:" + str(i+1) \
                              + " Testing Accuracy:" + str(correct * 100 / float(i+1))[:4] + "%")
     
+    def train(self, inputs, outputs, outputHelperSI, outputHelperIS, learning_rate):
+        
+        correct = 0
+        
+        start = time.time()
+        
+        for i in range(len(inputs)):
+            
+            # - FORWARD PASS -
+            self.load_input_layer(inputs[i])            
+            
+            layer_1_input_hidden = self.InputVector.dot(self.hiddenLayers[0])
+            layer_2_hiddin_output = self.sigmoid(layer_1_input_hidden.dot(self.outputLayer))
+            
+            # FIND ERROR
+            layer_2_error_hidden_output = layer_2_hiddin_output - outputHelperSI(outputs[i])
+            layer_2_error_delta = layer_2_error_hidden_output * self.sigmoid_derivative(layer_2_hiddin_output)
+            
+            # BACK PROPAGATION
+            layer_1_input_hidden_error = layer_2_error_delta.dot(self.outputLayer.T)
+            layer_1_error_delta = layer_1_input_hidden_error
+            
+            # update weights
+            self.outputLayer -= layer_1_input_hidden.T.dot(layer_2_error_delta) * learning_rate
+            self.hiddenLayers[0] -= self.InputVector.T.dot(layer_1_error_delta) * learning_rate
+            
+
+            if outputHelperIS(layer_2_hiddin_output) == outputs[i]:
+                correct += 1
+                
+            # For debug purposes, print out our prediction accuracy and speed 
+            # throughout the training process. 
+            elapsed_time = float(time.time() - start)
+            reviews_per_second = i / elapsed_time if elapsed_time > 0 else 0
+            
+            sys.stdout.write("\rProgress:" + str(100 * i/float(len(inputs)))[:4] \
+                             + "% Speed(reviews/sec):" + str(reviews_per_second)[0:5] \
+                             + " #Correct:" + str(correct) + " #Trained:" + str(i+1) \
+                             + " Training Accuracy:" + str(correct * 100 / float(i+1))[:4] + "%")
+            if(i % 2500 == 0):
+                print("")
+            
+            
+            
+        
+        
+            
+            
+            
+            
+            
--- movie_reviews/index.py
+++ movie_reviews/index.py
@@ -41,11 +41,23 @@ for i, word in enumerate(vocabList):
 
 def helperFunction(nnOutput):
     
-    if nnOutput >= 0.50:
+    if nnOutput >= 0.5:
         return 'positive'
     else:
         return 'negative'
     
-mlp = NeuralNetworkNLP( [ (1, len(vocabList)), (len(vocabList), 2), (2, 1) ], wordMap)
+def labelHelper(label):
+    
+    if label == 'negative':
+        return 0
+    else:
+        return 1
+    
+                            # input vector      # input layer       # hidden   # output layer
+mlp = NeuralNetworkNLP( [ (1, len(vocabList)), (len(vocabList), 10), (10, 1) ], wordMap)
 
-mlp.test(reviews[:-1000], labels[:-1000], helperFunction)
+#mlp.test(reviews[:-1000], labels[:-1000], helperFunction)
+
+mlp.train(reviews[:-1000], labels[:-1000], labelHelper, helperFunction, 0.01)
+
+#(1, 7000) (7000, 10) (10, 1)    (1, 10)
